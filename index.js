13ecfd0b4c1e4febe1401177a87930edb1a2a29f
Miki Masumoto <masumotomiki18@gmail.com>
2020-02-01 15:59:38 +0900

    Modify

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 $(".request_permission").on("click", function() {
+  console.log("DeviceMotionEvent :", DeviceMotionEvent);
+  console.log("DeviceOrientationEvent :", DeviceOrientationEvent);
   if (
     DeviceMotionEvent &&
     DeviceMotionEvent.requestPermission &&
@@ -14,5 +16,32 @@ $(".request_permission").on("click", function() {
     typeof DeviceOrientationEvent.requestPermission === "function"
   ) {
     DeviceOrientationEvent.requestPermission();
+  }
+});
+
+// AFRAME.registerComponent("rotation-reader", {
+//   tick: function() {
+//     var position = new THREE.Vector3();
+//     var quaternion = new THREE.Quaternion();
+
+//     return function() {
+//       this.el.object3D.getWorldPosition(position);
+//       this.el.object3D.getWorldQuaternion(quaternion);
+//       // position and rotation now contain vector and quaternion in world space.
+//     };
+//   }
+// });
+
+window.addEventListener("devicemotion", function(event) {
+  console.log(event.acceleration.x + " m/s2");
+  let camera = document.getElementById("camera");
+
+  if (camera) {
+    let position = camera.getAttribute("position");
+    let rotation = camera.getAttribute("rotation");
+
+    position.x += event.acceleration.x;
+    position.z += event.acceleration.y;
+    camera.setAttribute("position", position);
   }
 });
